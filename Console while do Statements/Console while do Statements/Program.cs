2e4f9edb35f45eebba8825a1e3e68a7dd531e333
LukeSometimesCodes <121363338+LukeSometimesCodes@users.noreply.github.com>
2023-04-04 20:32:15 -0600

    Add files via upload

---
+++ Console while do Statements/Console while do Statements/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_while_do_Statements
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("How old am I?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool ageGuessed = number == 22;
+
+            do
+            {
+                switch (number)
+                {
+                    case 45:
+                        Console.WriteLine("This is wrong, guess again");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 34:
+                        Console.WriteLine("This is wrong, guess again");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 60:
+                        Console.WriteLine("This is wrong, guess again");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 22:
+                        Console.WriteLine("Correct, good job!");
+                        ageGuessed = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("You are wrong, guess again");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                }
+            }
+            while (!ageGuessed);
+            { 
+            }
+            Console.ReadLine();
+        }
+    }
+}
