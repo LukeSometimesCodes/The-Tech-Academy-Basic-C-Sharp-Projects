e0113eb9b9a966498c3590cc3863d64f40fe97c8
LukeSometimesCodes <121363338+LukeSometimesCodes@users.noreply.github.com>
2023-04-05 16:17:50 -0600

    Add files via upload

---
+++ Console while do Statements/Console while do Statements/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_while_do_Statements
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("How old am I?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool ageGuessed = number == 22;
+
+            //do
+            //{
+            //    switch (number)
+            //    {
+            //        case 45:
+            //            Console.WriteLine("This is wrong, guess again");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+
+            //        case 34:
+            //            Console.WriteLine("This is wrong, guess again");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+
+            //        case 60:
+            //            Console.WriteLine("This is wrong, guess again");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+
+            //        case 22:
+            //            Console.WriteLine("Correct, good job!");
+            //            ageGuessed = true;
+            //            break;
+
+            //        default:
+            //            Console.WriteLine("You are wrong, guess again");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+
+            //    }
+            //}
+            //while (!ageGuessed);
+            //{ 
+            //}
+            //Console.ReadLine();
+
+
+            //WHILE STATEMENT BY ITSELF
+            while (!ageGuessed)
+            {
+                switch (number)
+                {
+                    case 60:
+                        Console.WriteLine("This is wrong, guess again");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 22:
+                        Console.WriteLine("Correct, good job!");
+                        ageGuessed = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("You are wrong, guess again");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                }
+            }Console.ReadLine();
+        }
+    }
+}
