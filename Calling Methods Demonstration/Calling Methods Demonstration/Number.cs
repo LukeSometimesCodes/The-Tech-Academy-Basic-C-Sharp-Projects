8eea6ef1d18ed15545037358b0af0aaad32b6928
LukeSometimesCodes <121363338+LukeSometimesCodes@users.noreply.github.com>
2023-04-10 18:54:20 -0600

    Add files via upload

---
+++ Calling Methods Demonstration/Calling Methods Demonstration/Number.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Demonstration
+{
+    public class Number
+    {
+
+        public int method1(int method)
+        {
+            return method + 5;
+        }
+
+        public int method2(int method)
+        {
+            return method - 1;
+        }
+
+        public int method3(int method)
+        {
+            return method * method;
+        }
+     
+        
+           
+    }
+    
+}
---
+++ Calling Methods Demonstration/Calling Methods Demonstration/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Demonstration
+{
+    class Program
+    {
+        static void Main()
+        {
+           
+            Console.WriteLine("Pick number to do math operations on");
+            int userNum = Int32.Parse(Console.ReadLine());
+
+            Number numbers = new Number();
+
+            numbers.method1(userNum);
+            Console.WriteLine(userNum + " plus 5 is " + numbers.method1(userNum));
+
+            numbers.method2(userNum);
+            Console.WriteLine(userNum + " minus one is " + numbers.method2(userNum));
+
+            numbers.method3(userNum);
+            Console.WriteLine(userNum + " times itself is " + numbers.method3(userNum));
+
+            Console.ReadLine();
+            
+
+
+
+            
+        }
+    }
+}
