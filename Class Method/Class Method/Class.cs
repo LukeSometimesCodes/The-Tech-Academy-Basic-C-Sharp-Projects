2b18da650a2b0fbb7ee0e51477c589f52713e19d
LukeSometimesCodes <121363338+LukeSometimesCodes@users.noreply.github.com>
2023-04-13 16:06:31 -0600

    Add files via upload

---
+++ Class Method/Class Method/Class.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method
+{
+    class Class
+    {
+        public void testMethod(int numOne, int numTwo)
+        {
+            int equation = numOne * numTwo;
+            int result = numTwo;
+            Console.WriteLine(result);
+
+        }
+        
+
+    }
+}
---
+++ Class Method/Class Method/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method
+{
+    class Program
+    {
+        static void Main()
+        {
+            Class number = new Class();
+
+            Console.WriteLine("Choose a number");
+            int numOne = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Choose a second number");
+            int numTwo = Int32.Parse(Console.ReadLine());
+
+            number.testMethod(numOne, numTwo);
+            number.testMethod(numOne: 5, numTwo:8) ;
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
