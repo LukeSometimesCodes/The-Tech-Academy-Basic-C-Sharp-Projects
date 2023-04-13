2d6170a8d722faab2a9249ef9d5a6ef41b754a2b
LukeSometimesCodes <121363338+LukeSometimesCodes@users.noreply.github.com>
2023-04-12 19:28:10 -0600

    Add files via upload

---
+++ Main Method/Main Method/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+
+            integer_intake numbers = new integer_intake();
+
+            
+            Console.WriteLine("Your number plus 25 is " + numbers.method(8));
+
+            
+            Console.WriteLine("Your decimal plus 20 is " + numbers.method(10.5m));
+
+            
+            
+            Console.WriteLine("Your string converted to integer plus 45 is " + numbers.method("11"));
+
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ Main Method/Main Method/integer intake.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    class integer_intake
+    {
+
+        public int method(int addition25)
+        {
+            return addition25 + 25;
+        }
+
+        public int method(decimal addition20)
+        {
+            int userNum = Convert.ToInt32(addition20);
+            return userNum + 20;
+        }
+
+        public int method(string addition45)
+        {
+            
+            return Convert.ToInt32(addition45) + 45;
+        }
+    }
+}
