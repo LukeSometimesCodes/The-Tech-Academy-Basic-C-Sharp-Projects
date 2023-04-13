b8ba1724a4ec2192537d16aad7f17685f8b8c7b5
LukeSometimesCodes <121363338+LukeSometimesCodes@users.noreply.github.com>
2023-04-12 19:29:09 -0600

    Add files via upload

---
+++ Method Demonstration/Method Demonstration/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Demonstration
+{
+    class Class1
+    {
+        public int method(int method, int method1 = 5)
+        {
+            return method * method1 ;
+        }
+    }
+}
---
+++ Method Demonstration/Method Demonstration/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Demonstration
+{
+    class Program
+    {
+        static void Main()
+        {
+            Class1 number = new Class1();
+            Console.WriteLine("Please pick a number");
+
+            int userNum = Int32.Parse(Console.ReadLine());
+            
+            try
+            {
+                Console.WriteLine("Please select another number, or don't enter one at all");
+                int userNum1 = Int32.Parse(Console.ReadLine());
+                Console.WriteLine(number.method(userNum, userNum1));
+
+
+                
+            }
+            catch
+            {
+                Console.WriteLine(number.method(userNum));
+            }
+            Console.ReadLine();
+           
+        }
+    }
+}
