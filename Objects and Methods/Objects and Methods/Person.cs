344688ea4b58615220a2f96727c6e38865beae67
LukeSometimesCodes <121363338+LukeSometimesCodes@users.noreply.github.com>
2023-04-14 17:47:54 -0600

    Add files via upload

---
+++ Objects and Methods/Objects and Methods/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects_and_Methods
+{
+    public class Person
+    {
+        string FirstName = "Luke";
+        string LastName = " Chevrolet";
+
+        public void SayName()
+        { 
+            Console.WriteLine("Name: " + FirstName + "" + LastName);
+        }
+
+        public void Sample()
+        {
+            string FirstName = "Sample";
+            string LastName = "Student";
+            Console.WriteLine(FirstName + " " + LastName);
+             
+        }
+    }
+}
---
+++ Objects and Methods/Objects and Methods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects_and_Methods
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.SayName();
+
+            employee.Sample();
+
+
+            Console.ReadLine();
+        }
+    }
+}
