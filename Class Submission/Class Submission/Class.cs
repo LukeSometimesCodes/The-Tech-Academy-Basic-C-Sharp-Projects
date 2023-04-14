6bc130fa38a4c52a2049a0e7102a79c8a2166918
LukeSometimesCodes <121363338+LukeSometimesCodes@users.noreply.github.com>
2023-04-14 17:16:32 -0600

    Add files via upload

---
+++ Class Submission/Class Submission/Class.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Submission
+{
+    class Class
+    {
+        public void Method(int userNum) //method that outputs a integer
+        {
+            int equation = userNum / 2;
+            Console.WriteLine(equation);
+        }
+
+
+        public void Thing(int userNum)
+        {
+
+            int.TryParse("5", out userNum);
+
+        }
+
+        //overload methods below 
+        public int overload(int method)
+        {
+            int numbertest = 10;
+            return numbertest * 5;
+        }
+
+        public int overload(decimal numbertest)
+        {
+            int method = (int)Convert.ToDecimal(numbertest);
+            return method + 5;
+        }
+
+        
+       
+
+
+    }
+
+   
+       
+
+
+}
