f4bab800921339b90328a93c1e7baebb9f9de32c
LukeSometimesCodes <121363338+LukeSometimesCodes@users.noreply.github.com>
2023-04-17 16:17:57 -0600

    Add files via upload

---
+++ Operators Submission/Operators Submission/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Submission
+{
+    public class Employee
+    {
+        string FirstName;
+        string LastName;
+        int ID = 5;
+
+        public static Employee operator ==(Employee employee1, Employee employee2)
+        {
+            string employeeName = "Luke";
+            string LastName = "Test";
+            int ID = 5;
+        }
+
+        public static Employee operator !=(Employee employee1, Employee employee2)
+        {
+            string employeeName = "Luke";
+            string LastName = "Test";
+            int ID = 5;
+        }
+    }
+
+    
+
+}
