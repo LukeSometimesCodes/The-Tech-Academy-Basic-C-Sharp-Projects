66490f3d5e059a8ab01ae4252544392476e4b35b
LukeSometimesCodes <121363338+LukeSometimesCodes@users.noreply.github.com>
2023-04-17 15:42:57 -0600

    Add files via upload

---
+++ Parsing Enums/Parsing Enums/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsing_Enums
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("What is the current day of the week?");
+                string userAnswer = Console.ReadLine();
+
+                Week week1;
+
+                if (Enum.TryParse<Week>(userAnswer, out week1))
+                    Console.WriteLine("{0} converted to {1}", userAnswer, week1);
+
+                else
+                {
+                    Console.WriteLine("Please enter an actual day of the week");
+                }
+            }
+            catch (InvalidCastException)
+            {
+                Console.WriteLine("Please enter an actual day of the week");
+            }
+            Console.ReadLine();
+        }
+
+            
+        public enum Week
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+
+        
+
+}   
+
---
+++ Parsing Enums/Parsing Enums/Week.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsing_Enums
+{
+    public class Week
+    {
+       //Dont mind this page
+    }
+
+    public enum dayOfWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
