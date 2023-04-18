1818402fc139f90fa5c9a0feb4d52005199115bb
LukeSometimesCodes <121363338+LukeSometimesCodes@users.noreply.github.com>
2023-04-17 19:44:58 -0600

    Add files via upload

---
+++ Abstract Class/Abstract Class/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class
+{
+    public class Employee : Person, IQuittable
+    {
+        public override void SayName(string FirstName, string LastName)
+        {
+
+   
+            Console.WriteLine(LastName + FirstName);
+        }
+
+        public void Quit()
+        {
+            throw new NotImplementedException();
+        }
+
+        //public void IQuittable() // for Polymorphism assignment
+        //{
+        //    Quit();
+        //}
+        
+    }
+}
