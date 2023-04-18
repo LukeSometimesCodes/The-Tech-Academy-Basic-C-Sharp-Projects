dfb4abd6b47132745e90b365eee9ec7a0e7537c4
LukeSometimesCodes <121363338+LukeSometimesCodes@users.noreply.github.com>
2023-04-18 15:48:46 -0600

    Add files via upload

---
+++ Constructor Submission/Constructor Submission/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string myName = "Luke";
+
+            var otherName = "notLuke";
+        }
+
+        class Test
+        {
+            private int id;
+            private string name;
+
+            public Test() : this(0, "Sample")
+            {
+            }
+
+            public Test(int id, string name)
+            {
+                this.id = 5;
+                this.name = "Luke";
+            }
+        }
+
+
+
+
+        }
+}
