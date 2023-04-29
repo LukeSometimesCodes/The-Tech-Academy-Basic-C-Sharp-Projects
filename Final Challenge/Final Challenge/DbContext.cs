e9d2cbe57c2d1ceb0d3d7fd3ff10244a8ae8b4eb
LukeSometimesCodes <121363338+LukeSometimesCodes@users.noreply.github.com>
2023-04-29 16:30:21 -0600

    removing stuff

---
+++ Final Challenge/Final Challenge/DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Challenge
+{
+    public class DbContext : IDisposable, System.Data.Entity.Infrastructure.IObjectContextAdapter
+    {
+        public ObjectContext ObjectContext => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
